b284ffa21e72d3870309e95b865a01eb3ac42be9
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-06-14 08:14:44 +0900

    Initial commit

---
+++ ImportPlaygrounds/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ImportPlaygrounds
+//
+//  Created by 廣瀬雄大 on 2017/06/14.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
---
+++ ImportPlaygrounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImportPlaygrounds
+//
+//  Created by 廣瀬雄大 on 2017/06/14.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "スターください → https://github.com/bannzai/"
+        label.sizeToFit()
+        view.addSubview(label)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+    }
+}
+
