34212d282f97dcdff4838177afb4e1a79612617f
bannzai <hiroseyuudai@hiroseyudais-MacBookPro.local>
2017-06-14 09:13:58 +0900

    WIP add framework and build next add view controller

---
+++ ImportPlaygroundsFramework/ImportPlaygroundsFramework.h
@@ -0,0 +1,19 @@
+//
+//  ImportPlaygroundsFramework.h
+//  ImportPlaygroundsFramework
+//
+//  Created by 廣瀬雄大 on 2017/06/14.
+//  Copyright © 2017年 廣瀬雄大. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImportPlaygroundsFramework.
+FOUNDATION_EXPORT double ImportPlaygroundsFrameworkVersionNumber;
+
+//! Project version string for ImportPlaygroundsFramework.
+FOUNDATION_EXPORT const unsigned char ImportPlaygroundsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImportPlaygroundsFramework/PublicHeader.h>
+
+
